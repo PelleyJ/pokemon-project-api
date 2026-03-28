dc9b813cce51ccddccaf286af38c1548310c9719
PelleyJ <pelleysam@gmail.com>
2026-03-28 23:26:30 +0900

    fixed missing user for swagger ui

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -2,7 +2,61 @@ const express = require("express");
 const router = express.Router();
 const userController = require("../controllers/userController");
 
+/**
+ * @swagger
+ * /users/register:
+ *   post:
+ *     summary: Register a new user
+ *     description: Create a new user with hashed password
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             required:
+ *               - username
+ *               - email
+ *               - password
+ *             properties:
+ *               username:
+ *                 type: string
+ *               email:
+ *                 type: string
+ *               password:
+ *                 type: string
+ *     responses:
+ *       201:
+ *         description: User created successfully
+ */
 router.post("/register", userController.registerUser);
+
+/**
+ * @swagger
+ * /users/login:
+ *   post:
+ *     summary: Login user
+ *     description: Authenticate user and return JWT token
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             required:
+ *               - email
+ *               - password
+ *             properties:
+ *               email:
+ *                 type: string
+ *               password:
+ *                 type: string
+ *     responses:
+ *       200:
+ *         description: Login successful, returns token
+ *       401:
+ *         description: Invalid credentials
+ */
 router.post("/login", userController.loginUser);
 
 module.exports = router;
